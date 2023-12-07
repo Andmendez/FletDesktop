9979ec771016e8b4844c912338d1e103aae8eeda
Andres Felipe Forero Mendez <andmendezfel@gmail.com>
2023-12-06 23:18:04 -0500

    Update eventos.py

--- eventos.py
+++ eventos.py
@@ -13,15 +13,23 @@ def main(page = ft.Page):
 
 #Al darle click al boton add una checkbox con el nombre que se le dio en el textField.
 
-    def add_clicked(e):
-        page.add(ft.Checkbox(label=new_task.value))
-        new_task.value = ""
-        new_task.focus()
-        new_task.update()
+    # def add_clicked(e):
+    #     page.add(ft.Checkbox(label=new_task.value))
+    #     new_task.value = ""
+    #     new_task.focus()
+    #     new_task.update()
 
-    new_task = ft.TextField(hint_text="Whats needs to be done?", width=300)
-    page.add(ft.Row([new_task, ft.ElevatedButton("Add", on_click=add_clicked)]))
-    page.update()
+    # new_task = ft.TextField(hint_text="Whats needs to be done?", width=300)
+    # page.add(ft.Row([new_task, ft.ElevatedButton("Add", on_click=add_clicked)]))
+    # page.update()
+### //////////////////////////////////////////////////////////////////////////////////////// ###
+
+#En este caso se deshabilita una de nuestras entradas impidiendo presionar con el mouse.
+    first_name = ft.TextField()
+    last_name = ft.TextField()
+    first_name.disabled = False
+    last_name.disabled = True
+    page.add(first_name, last_name)
 
 
 ft.app(target=main)
